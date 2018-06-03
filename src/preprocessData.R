4031a3b3bce20eb028602d1b5dfb77b422cf80e2
DeathTost <deathtost@gmail.com>
2018-06-03 21:04:07 +0200

    Sprzatanie kodu preprocessingu

--- src/preprocessData.R
+++ src/preprocessData.R
@@ -1,5 +1,5 @@
-preprocessData <- function(dataSet){
-
+preprocessData <- function(dataSet)
+{ 
 # check NA
   column.has.na <- apply(dataSet, 2, function(x){any(is.na(x))})
   column.has.na[column.has.na]
@@ -11,11 +11,8 @@ preprocessData <- function(dataSet){
 
 #   changing NAs to 0s for position scores 
   dataSet[, 68:94][is.na(dataSet[, 68:94])] <- 0
-# column.has.na <- apply(dataSet, 2, function(x){any(is.na(x))})
-# sum(column.has.na)
 
 #   4 columns with NAs left: club, club_logo, league, eur_release_clause
-
 #   fill club column with "None" instead of NAs
   dataSet[, 4][is.na(dataSet[, 4])] <- "None"
 #   remove remaining columns with NAs
@@ -40,42 +37,39 @@ preprocessData <- function(dataSet){
   dataSet$prefers_front <- apply(dataSet[,c('prefers_rs','prefers_rw','prefers_st','prefers_lw','prefers_cf','prefers_ls')] == "True", 1, any)
   dataSet$prefers_mid <- apply(dataSet[,c('prefers_rf','prefers_ram','prefers_rcm','prefers_rm','prefers_rdm','prefers_cam','prefers_cm','prefers_lm','prefers_cdm','prefers_lf','prefers_lam','prefers_lcm','prefers_ldm')] == "True", 1, any)
   dataSet$prefers_back <- apply(dataSet[,c('prefers_rcb','prefers_rb','prefers_rwb','prefers_cb','prefers_lb','prefers_lwb','prefers_lcb')] == "True", 1, any)
-  dataSet <- dataSet[, !(names(dataSet) %in% c('prefers_rs','prefers_rw','prefers_st','prefers_lw','prefers_cf','prefers_ls','prefers_rf','prefers_ram','prefers_rcm','prefers_rm','prefers_rdm','prefers_cam','prefers_cm','prefers_lm','prefers_cdm','prefers_lf','prefers_lam','prefers_lcm','prefers_ldm','prefers_rcb','prefers_rb','prefers_rwb','prefers_cb','prefers_lb','prefers_lwb','prefers_lcb'))]
+  dataSet <- dataSet[, !(names(dataSet) %in% c('prefers_gk','prefers_rs','prefers_rw','prefers_st','prefers_lw','prefers_cf','prefers_ls','prefers_rf','prefers_ram','prefers_rcm','prefers_rm','prefers_rdm','prefers_cam','prefers_cm','prefers_lm','prefers_cdm','prefers_lf','prefers_lam','prefers_lcm','prefers_ldm','prefers_rcb','prefers_rb','prefers_rwb','prefers_cb','prefers_lb','prefers_lwb','prefers_lcb'))]
   dataSet <- dataSet[, !grepl("speciality|trait", colnames(dataSet))]
   
   dataSet[,which(grepl("prefers", colnames(dataSet)))] <- lapply(dataSet[,which(grepl("prefers", colnames(dataSet)))], as.logical)
-  #  sum(apply(dataSet[,c('prefers_front','prefers_mid','prefers_back')],1,all))
-#  dataSet = subset(dataSet,apply(dataSet[,c('prefers_front','prefers_mid','prefers_back')],1,all))
-
 #   position scores
-  dataSet$score_front <- apply(dataSet[,c('rs','rw','st','lw','cf','ls')], 1, mean)
+
+  dataSet$score_front <- apply(dataSet[,c('rs','rw','lw','st','cf','ls')], 1, mean)
   dataSet$score_mid <- apply(dataSet[,c('rf','ram','rcm','rm','rdm','cam','cm','lm','cdm','lf','lam','lcm','ldm')], 1, mean)
   dataSet$score_back <- apply(dataSet[,c('rcb','rb','rwb','cb','lb','lwb','lcb')], 1, mean)
   dataSet <- dataSet[, !(names(dataSet) %in% c('rs','rw','st','lw','cf','ls','rf','ram','rcm','rm','rdm','cam','cm','lm','cdm','lf','lam','lcm','ldm','rcb','rb','rwb','cb','lb','lwb','lcb'))]
   
-  classes <- matrix("",nrow = nrow(dataSet),ncol = 1)
-  minimum_score_diff = 5
-  for (i in 1:nrow(dataSet)) {
-    class = "Any"
-    if (dataSet$gk[i] != 0){
-      class = "Goalkeeper"
-      classes[i] = class
-      next
-    }
-    if (dataSet$score_front[i] > dataSet$score_mid[i] && dataSet$score_front[i] > dataSet$score_back[i] && abs(dataSet$score_front[i] - dataSet$score_mid[i]) > minimum_score_diff && abs(dataSet$score_front[i] - dataSet$score_back[i]) > minimum_score_diff){
-      class = "Front"
-    }
-    
-    if (dataSet$score_mid[i] > dataSet$score_front[i] && dataSet$score_mid[i] > dataSet$score_back[i] && abs(dataSet$score_front[i] - dataSet$score_mid[i]) > minimum_score_diff && abs(dataSet$score_mid[i] - dataSet$score_back[i]) > minimum_score_diff){
-      class = "Mid"
-    }
-    
-    if (dataSet$score_back[i] > dataSet$score_mid[i] && dataSet$score_back[i] > dataSet$score_front[i] && abs(dataSet$score_back[i] - dataSet$score_mid[i]) > minimum_score_diff && abs(dataSet$score_front[i] - dataSet$score_back[i]) > minimum_score_diff){
-      class = "Back"
-    }
-    classes[i] = class
-  }
-  dataSet$label <- classes
+  minimum_score_diff = 3 #5
+  ######
+  dataSet$label <- as.factor(with(dataSet, 
+                              ifelse(gk != 0,
+                                     "Goalkeeper",
+                                      ifelse(score_front > score_mid &
+                                            score_front > score_back & 
+                                            abs(score_front - score_mid) > minimum_score_diff & 
+                                            abs(score_front - score_back) > minimum_score_diff,
+                                      "Front",
+                                      ifelse(score_mid > score_front &
+                                            score_mid > score_back & 
+                                            abs(score_front - score_mid) > minimum_score_diff &
+                                            abs(score_mid - score_back) > minimum_score_diff, 
+                                      "Mid",
+                                      ifelse( score_back > score_mid &
+                                              score_back > score_front &
+                                              abs(score_back - score_mid) > minimum_score_diff &
+                                              abs(score_back - score_front) > minimum_score_diff,
+                                      "Back",
+                                      "Any"))))))
+  table(dataSet$label)
   
   dataSet$club <- as.factor(dataSet$club)
   dataSet$nationality <- as.factor(dataSet$nationality)
