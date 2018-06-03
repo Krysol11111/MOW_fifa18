cdfee8723aa80f534c6bff0c86429b80136c2abb
DeathTost <deathtost@gmail.com>
2018-06-03 21:04:33 +0200

    Sprzatanie selekcji

--- src/selection.R
+++ src/selection.R
@@ -1,26 +1,20 @@
-# get data as input
-# compute correlation matrix for attributes, watch out for attributes types because we have 
-# numerical, logical and character data
-# find correlation between attributes and remove redundant examples
-# use pca to reduce the number of attributes
-# use ranking method (or somethin else) to evaluate attributes and select the best subset
-# return data with selected attributes
+library(readr)
+library(lattice)
+library(mlbench)
+library(ggplot2)
+library(caret)
+library(ranger)
+library(Boruta)
 
-selectedAttributes <- function(dataSet, classColumn){
-  library(readr)
-  library(lattice)
-  library(mlbench)
-  library(ggplot2)
-  library(caret)
-  #which(sapply(complete, is.factor))
-  library(ranger)
-  library(Boruta)
-  #dataSet$label
-  borutaCorrelation <- Boruta(data.frame(dataSet),classColumn,doTrace = 2,maxRuns = 25)
+selectedAttributes <- function(dataSet, classColumn)
+{
+  # get data as input
+  borutaCorrelation <- Boruta(data.frame(dataSet),classColumn,doTrace = 2,maxRuns = 100)
   finalBoruta <- TentativeRoughFix(borutaCorrelation)
   stats <- attStats(finalBoruta)
   
-  #correlationMatrix <- cor(complete[,-which(sapply(complete,is.character))])
+  # compute correlation matrix for attributes, watch out for attributes types because we have 
+  # numerical, logical and character data
   correlationMatrix <- cor(dataSet[sapply(dataSet, is.numeric)])
   #print(correlationMatrix)
   # find attributes that are highly corrected (ideally >0.75)
@@ -28,7 +22,9 @@ selectedAttributes <- function(dataSet, classColumn){
   # print indexes of highly correlated attributes
   #print(highlyCorrelated)
   
-  dataSet <- dataSet[,stats$medianImp > 16.0]
-  #complete <- complete[,stats$decision == "Confirmed"]
+  # find correlation between attributes and remove redundant examples
+  # use ranking method (or somethin else) to evaluate attributes and select the best subset
+  dataSet <- dataSet[,stats$medianImp > 8.0]
+  # return data with selected attributes
   return(dataSet)
 }
